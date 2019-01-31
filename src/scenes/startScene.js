bda07eec2581702fbcc5f6b357850ad337575c10
Wulfsbach <etemelkos@gmail.com>
2019-01-31 14:46:35 -0500

    added new assets, and animations to said assets, I believe the title screen is around 75% complete

--- src/scenes/startScene.js
+++ src/scenes/startScene.js
@@ -19,6 +19,7 @@ this.load.audio('treeoflife','assets/Sound/Music/treeoflife.wav');
 this.load.audio('OrbWhoosh','assets/Sound/FX/OrbWhoosh.wav');
 this.load.audio('OrbBoom','assets/Sound/FX/OrbBoom.wav');
 this.load.image('title', 'assets/Images/KorTitle.png');
+this.load.audio('burning', 'assets/Sound/FX/Burning.wav');
 this.load.image('orb1','assets/Images/SpriteSheets/OrbLoop/orb1.png');
 this.load.image('orb2','assets/Images/SpriteSheets/OrbLoop/orb2.png');
 this.load.image('orb3','assets/Images/SpriteSheets/OrbLoop/orb3.png');
@@ -35,6 +36,32 @@ this.load.image('orb13','assets/Images/SpriteSheets/OrbLoop/orb13.png');
 this.load.image('orb14','assets/Images/SpriteSheets/OrbLoop/orb14.png');
 this.load.image('orb15','assets/Images/SpriteSheets/OrbLoop/orb15.png');
 this.load.image('orb16','assets/Images/SpriteSheets/OrbLoop/orb16.png');
+this.load.image('Explosion1','assets/Images/SpriteSheets/Explosion6/Explosion1.png');
+this.load.image('Explosion2','assets/Images/SpriteSheets/Explosion6/Explosion2.png');
+this.load.image('Explosion3','assets/Images/SpriteSheets/Explosion6/Explosion3.png');
+this.load.image('Explosion4','assets/Images/SpriteSheets/Explosion6/Explosion4.png');
+this.load.image('Explosion5','assets/Images/SpriteSheets/Explosion6/Explosion5.png');
+this.load.image('Explosion6','assets/Images/SpriteSheets/Explosion6/Explosion6.png');
+this.load.image('Explosion7','assets/Images/SpriteSheets/Explosion6/Explosion7.png');
+this.load.image('Explosion8','assets/Images/SpriteSheets/Explosion6/Explosion8.png');
+this.load.image('Explosion9','assets/Images/SpriteSheets/Explosion6/Explosion9.png');
+this.load.image('Explosion10','assets/Images/SpriteSheets/Explosion6/Explosion10.png');
+this.load.image('Explosion11','assets/Images/SpriteSheets/Explosion6/Explosion11.png');
+this.load.image('Explosion12','assets/Images/SpriteSheets/Explosion6/Explosion12.png');
+this.load.image('Explosion13','assets/Images/SpriteSheets/Explosion6/Explosion13.png');
+this.load.image('Explosion14','assets/Images/SpriteSheets/Explosion6/Explosion14.png');
+this.load.image('Explosion15','assets/Images/SpriteSheets/Explosion6/Explosion15.png');
+this.load.image('Explosion16','assets/Images/SpriteSheets/Explosion6/Explosion16.png');
+this.load.image('Fire5-1', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-1.png');
+this.load.image('Fire5-2', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-2.png');
+this.load.image('Fire5-3', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-3.png');
+this.load.image('Fire5-4', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-4.png');
+this.load.image('Fire5-5', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-5.png');
+this.load.image('Fire5-6', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-6.png');
+this.load.image('Fire5-7', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-7.png');
+this.load.image('Fire5-8', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-8.png');
+this.load.image('Fire5-9', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-9.png');
+this.load.image('Fire5-10', 'assets/Images/SpriteSheets/Fire5Loop/Fire5-10.png');
 
 
 
@@ -66,6 +93,31 @@ this.anims.create({
     ],
     frameRate:8,
     repeat:-1
+});
+
+this.anime.create({
+    key:'exploding',
+    frames:[
+        {key: 'Explosion1'},
+        {key: 'Explosion2'},
+        {key: 'Explosion3'},
+        {key: 'Explosion4'},
+        {key: 'Explosion5'},
+        {key: 'Explosion6'},
+        {key: 'Explosion7'},
+        {key: 'Explosion8'},
+        {key: 'Explosion9'},
+        {key: 'Explosion10'},
+        {key: 'Explosion11'},
+        {key: 'Explosion12'},
+        {key: 'Explosion13'},
+        {key: 'Explosion14'},
+        {key: 'Explosion15'},
+        {key: 'Explosion16'},
+    ],
+    frameRate:5,
+    repeat:-1
+    
 });
 
 
@@ -131,7 +183,7 @@ titleMusic.play();
 
 var title = this.add.image(600,100,'title');
 
-
+// this.cameras.main.setBounds(0,0, map.widthInPixels, map.heightInPixels);
 var tween = this.tweens.add({
     targets:orbCrash,
     props:{
@@ -142,7 +194,7 @@ var tween = this.tweens.add({
     },
     duration:4000,
     onStart: function() {OrbFX.play()},
-    onComplete: function() {Boom.play()}
+    onComplete: function() {Boom.play(),}
     
     
     
@@ -152,7 +204,9 @@ var tween = this.tweens.add({
      
  }
 
-
+orbEnding(){
+    
+};
 
 
 
